8be99af970b2e860298e4de3c1f85f05d51c37cc
KainaK0 <johancb1411@gmail.com>
2018-05-14 19:36:51 -0500

    Encoding categorical data, changing string data by numerical

--- data_preprocessing_template_me.py
+++ data_preprocessing_template_me.py
@@ -15,12 +15,17 @@ X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,-1].values
 
 #Taking care of missing Data
-
 from sklearn.preprocessing import Imputer  #import of the class
 imputer = Imputer(missing_values = 'NaN',strategy = 'mean',axis = 0) #Creating the object
 imputer = imputer.fit(X[:,1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder  #import of the class
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features= [0])
+X = onehotencoder.fit_transform(X).toarray()
 
-
-
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
